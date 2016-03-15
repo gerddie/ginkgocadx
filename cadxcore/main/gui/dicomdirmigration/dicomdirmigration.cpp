320b3548f67a9d03b3d1558f96ea95556e6c596a
Gert Wollny <gw.fossdev@gmail.com>
2016-03-15 19:07:27 +0100

    Add check of wxMkdir return value, Related: #15, Closes COV-74508

--- cadxcore/main/gui/dicomdirmigration/dicomdirmigration.cpp
+++ cadxcore/main/gui/dicomdirmigration/dicomdirmigration.cpp
@@ -130,12 +130,13 @@ wxString GNC::GUI::DicomDirMigrationDialog::SelectFolder()
 			if(answer == wxNO) {
 				return wxEmptyString;
 			} else {
+                bool success;
 	#ifdef _WIN32
-				wxMkdir(path,511);
+                success = wxMkdir(path,511);
 	#else
-				wxMkDir(path.ToUTF8(), 0770);
+                success = wxMkDir(path.ToUTF8(), 0770);
 	#endif
-				if(!wxDirExists(path))
+                if(!success || !wxDirExists(path))
 				{
 					wxMessageBox(_("Failed to create directory, make sure the path is correct and you have permissions on parent directory"), _("Info"));
 					return wxEmptyString;
