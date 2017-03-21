012f4fb954725f78d0f7e54769d8738b3950777b
Gert Wollny <gw.fossdev@gmail.com>
2017-03-21 15:08:21 +0100

    maintain vtk-opengl code path compatibility

--- cadxcore/vtk/gl.h
+++ cadxcore/vtk/gl.h
@@ -1,5 +1,5 @@
-#ifndef vtkgl_h
-#define vtkgl_h
+#ifndef ginkgo_vtkgl_h
+#define ginkgo_vtkgl_h
 
 #ifdef VTK_RENDERING_OPENGL2
 #define GL_GLEXT_PROTOTYPES 1
@@ -8,6 +8,22 @@
 #else 
 #include <vtkOpenGLExtensionManager.h>
 #include <vtkgl.h>
+// this is really a bad hack
+using namespace vtkgl;
+#define glCreateProgram() vtkgl::CreateProgram()
+#define glCreateShader(X) vtkgl::CreateShader(X)
+#define glShaderSource(A,B,C,D) vtkgl::ShaderSource(A,B,C,D)
+#define glCompileShader(X) vtkgl::CompileShader(X)
+#define glAttachShader(X,Y) vtkgl::AttachShader(X,Y)
+#define glGetShaderiv(A, B, C) vtkgl::GetShaderiv(A,B,C)
+#define glGetShaderInfoLog(A,B,C,D) vtkgl::GetShaderInfoLog(A,B,C,D)
+#define glLinkProgram(A) vtkgl::LinkProgram(A)
+#define glUseProgram(A) vtkgl::UseProgram(A)
+#define glGetProgramiv(A,B,C) vtkgl::GetProgramiv(A,B,C)
+#define glGetUniformLocation vtkgl::GetUniformLocation
+#define glUniform1i(A,B)  vtkgl::Uniform1i(A,B)
+#define glUniform1f(A,B)  vtkgl::Uniform1f(A,B)
+
 #endif
 
 #endif 
